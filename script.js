53824da780f416b1378f7b6f18b968cf751e29b8
F2000 <landry.kengni@gmail.com>
2026-05-01 03:46:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const images = [
+    'travel_pics/africa.png',
+    'travel_pics/orient.png',
+    'travel_pics/london.png',
+    'travel_pics/usa.png',
+    'travel_pics/asie.png',
+    'travel_pics/airplane.png'
+];
+
+let index = 0;
+const hero = document.getElementsByClassName('hero')[0];
+
+function changebackground() {
+    hero.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.6)), url(${images[index]})`;
+    /*hero.style.backgroundRepeat = "no-repeat";
+    hero.style.backgroundPosition = "center";
+    hero.style.backgroundSize = "cover";*/
+
+    index = (index + 1) % images.length;
+}
+
+changebackground();
+setInterval(changebackground, 5000);
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+const revealElements = document.querySelectorAll('.reveal');
+revealElements.forEach((el) => observer.observe(el));
+
+let currentTestimonial = 0;
+const testimonialCards = document.querySelectorAll('.testimonial-card');
+
+function showTestimonial(index) {
+    testimonialCards.forEach(card => card.classList.remove('active'));
+    currentTestimonial = (index + testimonialCards.length) % testimonialCards.length;
+    testimonialCards[currentTestimonial].classList.add('active');
+}
+
+function changeTestimonial(step) {
+    showTestimonial(currentTestimonial + step);
+    resetTimer();
+}
+
+let testimonialTimer = setInterval(() => changeTestimonial(1), 10000);
+
+function resetTimer() {
+    clearInterval(testimonialTimer);
+    testimonialTimer = setInterval(() => changeTestimonial(1), 10000);
+}
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.getElementById('nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('is-active');
+})
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const nom = document.querySelector('input[placeholder="Votre Nom"]').value;
+    const contact = document.querySelector('input[placeholder="Votre Contact"]').value;
+    const email = document.querySelector('input[placeholder="Votre Email"]').value;
+    const projet = document.querySelector('textarea').value;
+
+    const message = `Bonjour Gonga Voyages! %0A%0A` + `Nom : ${nom}%0A` + `Contact : ${contact}%0A` + `E-mail : ${email}%0A` `${projet}`;
+
+    const telephone = "237697139458";
+
+    window.open(`https://wa.me/${telephone}?text=${message}`, '_blank');
+});
